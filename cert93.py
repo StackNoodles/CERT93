2ec57e56f4c06f890a2bdbaf5238f6cd0eaec065
Yanni Haddar <yanhaddar@gmail.com>
2022-12-01 18:26:55 -0500

    removed win32 import temporarely

--- cert93.py
+++ cert93.py
@@ -8,9 +8,9 @@ import resources
 import settings
 import time
 
-import win32api
-import win32con
-import win32gui
+#import win32api
+#import win32con
+#import win32gui
 
 
 from tkinter import *
@@ -28,15 +28,16 @@ def __run_game() -> None:
 
     # Initialisation de la fenetre a la bonne taille, sans frame
     screen = pygame.display.set_mode((settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT), pygame.NOFRAME)
-    fuchsia = (87, 71, 30)  # Definir le fuschia comme la couleur a rendre transparente
+    fuchsia = (24, 32, 48)  # Definir le fuschia comme la couleur a rendre transparente (une couleur peu commune)
     screen.fill(fuchsia) # Remplir le background de fuschia
 
     hwnd = pygame.display.get_wm_info()["window"] # Recuperer le handle de la fenetre
     # Mettre la fenetre en mode layered (permet la transparence et meilleur pour les jeux/medias)
-    win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE, win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
+    ###################win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE, win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
+    
     # Set les attribus suivants sur la fenetre layered:
     # la couleur cle est fuchsia, l'opacite de cette couleur est 0, et le mode est transparence par cle
-    win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*fuchsia), 0, win32con.LWA_COLORKEY)
+    ###################win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*fuchsia), 0, win32con.LWA_COLORKEY)
 
     # Splash logo dev
     splash_screen("img\logo_stacknoodles_transparent.png", 2, screen)
@@ -45,7 +46,7 @@ def __run_game() -> None:
     splash_screen("img\logo_cert93.png", 3, screen)
 
     # Enlever les settings d'opacité à la fenêtre layered
-    win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*fuchsia), 255, win32con.LWA_ALPHA)
+    #####################win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*fuchsia), 255, win32con.LWA_ALPHA)
     # Création de la fenêtre de jeu
     pygame.display.set_caption("CERT-93")
     screen = pygame.display.set_mode((settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT))
